91aefaaedb6f66bcbe3e8915069ed67b61e6efee
natalialuzuriaga <nat.luzuriaga@gmail.com>
2023-04-22 15:06:15 -0700

    created activity card component

---
+++ src/ActivityCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Card, CardBody, CardFooter, Heading, Image, Stack, Text } from '@chakra-ui/react'
+
+const ActivityCard = (props) => {
+    return (
+        <Card
+            direction={{ base: 'column', sm: 'row' }}
+            overflow='hidden'
+            variant='outline'
+            size="md"
+            >
+            <Image
+                objectFit='cover'
+                maxW={{ base: '100%', sm: '200px' }}
+                src='https://assets.website-files.com/62aba60b94cb7948698926f0/6307becb922f3daccae12877_Blog%20-%20Hollywood%20Sign-Main%20(7)-p-800.jpg'
+                alt='Caffe Latte'
+            />
+            <Stack>
+                <CardBody>
+                <Heading size='md'>Hollywood Hike</Heading>
+
+                <Text py='2'>
+                The Hollywood Hike is a popular hiking trail in the Hollywood Hills of Los Angeles, California. It begins at the Griffith Observatory and winds its way up to the Hollywood Sign, offering spectacular views of the city and the iconic sign along the way. The trail is approximately 3 miles long and is rated as moderate in difficulty, making it suitable for most hikers.
+                </Text>
+                </CardBody>
+
+                {/* <CardFooter>
+                <Button variant='solid' colorScheme='blue'>
+                    Buy Latte
+                </Button>
+                </CardFooter> */}
+            </Stack>
+            </Card>
+    )
+}
+
+export default ActivityCard;
--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import {
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
+import ActivityCard from "./ActivityCard";
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
@@ -19,19 +21,9 @@ function App() {
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+            <ActivityCard/>
+            <ActivityCard/>
+            <ActivityCard/>
           </VStack>
         </Grid>
       </Box>
